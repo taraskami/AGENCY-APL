8c523b30a5f659d25a1778c093034049db0d2964
Ken Tanzer <ken.tanzer@gmail.com>
2010-04-22 23:42:35 -0700

    Adds report for permisions assigned.

--- database/pg/db_mods/db_mod.15.add.permission.report.sql
+++ database/pg/db_mods/db_mod.15.add.permission.report.sql
@@ -1,8 +1,26 @@
-/*
-Simple Staff Report
-*/
+BEGIN;
 
-INSERT INTO tbl_report (report_title, report_category_code, report_header, report_footer, report_comment, client_page, allow_output_screen, allow_output_spreadsheet, override_sql_security, rows_per_page, output, report_permission, sql, variables, added_by, added_at, changed_by, changed_at, is_deleted, deleted_at, deleted_by, deleted_comment, sys_log) VALUES ('Simple staff report', 'GENERAL', 'Simple staff report', NULL, NULL, NULL, true, true, false, NULL, NULL, NULL, 'SELECT staff_id,staff_program(staff_id),staff_project(staff_id) FROM staff', NULL, 1, current_timestamp, 1, current_timestamp, false, NULL, NULL, NULL, NULL);
+INSERT INTO tbl_db_revision_history 
+	(db_revision_code,
+	db_revision_description,
+	agency_flavor_code,
+	git_sha,
+	git_tag,
+	applied_at,
+	comment,
+	added_by,
+	changed_by)
+
+	 VALUES ('ADD_PERMISSION_REPORT',
+			'Add permissions assigned report to database',
+			'AGENCY_CORE',
+			'',
+			'db_mod.15',
+			current_timestamp,
+			'', 
+			sys_user(),
+			sys_user()
+		  );
 
 INSERT INTO tbl_report (report_title, report_category_code, report_header, report_footer, report_comment, client_page, allow_output_screen, allow_output_spreadsheet, override_sql_security, rows_per_page, output, report_permission, sql, variables, added_by, changed_by, is_deleted, deleted_at, deleted_by, deleted_comment, sys_log) VALUES ('Permissions Assigned', 'GENERAL', 'AGENCY Permissions Report', NULL, 'This report lists all the permissions that have been assigned, from the permission table', NULL, true, true, false, NULL, NULL, 'admin', 'SELECT 
   permission_date,
@@ -18,4 +36,4 @@ INSERT INTO tbl_report (report_title, report_category_code, report_header, repor
   staff_id
   FROM permission', NULL, sys_user(), sys_user(), false, NULL, NULL, NULL, NULL);
 
-
+/* COMMIT; */
