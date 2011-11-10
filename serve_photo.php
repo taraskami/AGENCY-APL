980254c7a738cd17d33b31bdfad5bc1204f6c2ea
Ken Tanzer <ken.tanzer@gmail.com>
2011-11-09 22:10:35 -0800

    Missed the serve_photo script for photo patch

---
+++ serve_photo.php
@@ -0,0 +1,39 @@
+<?php
+
+$quiet='Y';
+include 'includes.php';
+//include 'photo.php';
+global $AG_CLIENT_PHOTO_BY_FILE,$AG_DEMO_MODE,$AG_IMAGES;
+$error='';
+$id=$_REQUEST['id'];
+$object=strtolower($_REQUEST['object']);
+$scale=orr($_REQUEST['scale'],1);
+
+if (!(is_valid($id,'integer') and is_numeric($scale) and in_array($object,array_keys($engine)))) {
+	$error[]="Invalid request passed to serve_photo.php. Object: $object, id: $id, scale: $scale";
+}
+if (!(has_perm($engine[$object]['perm_view']))) {
+	$error[]= "You don't have permission to view $object objects";
+}
+
+if ($error) {
+	log_error(implode($oline,$error));
+	page_close(TRUE);//silent
+	exit;
+}
+
+if ((!has_perm('photo')) or $AG_DEMO_MODE) {
+	if ($AG_DEMO_MODE) {
+		$filename=$AG_HOME_URL . $AG_IMAGES['DEMO_PHOTO'];
+	} else {
+		$filename=$AG_HOME_URL . $AG_IMAGES['NO_PHOTO'];
+	}
+	return serve_photo(file_get_contents($filename));
+}
+$filename=object_photo_filename($id,$object,$scale);
+//outline("trying for $filename");
+if ( $photo=@file_get_contents($filename)) {
+//outline("going for $filename");
+	return serve_photo($photo);
+}
+?>
