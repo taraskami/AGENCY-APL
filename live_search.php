0f7201cddeed5e677ff036a7186246b5779d0027
Ken Tanzer <ken.tanzer@gmail.com>
2012-03-02 01:14:57 -0800

    Fix to live_search alias fix.

--- live_search.php
+++ live_search.php
@@ -12,10 +12,14 @@ preg_match('/^[a-z_ ,0-9()]*$/i',$type) or die("Bad term passed");
 $def=get_def($type);
 
 if (has_perm($def['perm_view'])) {
-	$filter=array("ILIKE:{$type}_name({$type}_id) || ' (' || {$type}_id ||') ' || name_alias"=>'%'.$term.'%');
+	$alias = ($type==AG_MAIN_OBJECT_DB)
+		? " || COALESCE(name_alias,'')"
+		: '';
+	$filter=array("ILIKE:{$type}_name({$type}_id) || ' (' || {$type}_id ||') ' $alias"=>'%'.$term.'%');
 	$name = ( $GLOBALS['AG_DEMO_MODE'] and ($type==AG_MAIN_OBJECT_DB))
 		? "'XXXXXX'"
 		: $type . '_name('.$type.'_id)';
+//toggle_query_display();
 	$results=sql_fetch_column(agency_query("SELECT $name || ' (' || {$type}_id || ')' AS {$type}_name FROM {$type}",$filter,NULL,100),"{$type}_name");
 	echo(json_encode($results));
 }
