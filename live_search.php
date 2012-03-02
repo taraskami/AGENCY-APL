96a613027f660a2320282c84d3377547dc182b0d
Ken Tanzer <ken.tanzer@gmail.com>
2012-03-01 17:42:57 -0800

    Look at aliases for live search (client QS & entry_browse)

--- live_search.php
+++ live_search.php
@@ -7,12 +7,12 @@ include 'includes.php';
 $term=$_REQUEST['term'];
 $type=$_REQUEST['type'];
 preg_match('/^[a-z_]*$/i',$type) or die("Bad type passed");
-preg_match('/^[a-z_ 0-9()]*$/i',$type) or die("Bad term passed");
+preg_match('/^[a-z_ ,0-9()]*$/i',$type) or die("Bad term passed");
 
 $def=get_def($type);
 
 if (has_perm($def['perm_view'])) {
-	$filter=array("ILIKE:{$type}_name({$type}_id) || ' (' || {$type}_id ||')'"=>'%'.$term.'%');
+	$filter=array("ILIKE:{$type}_name({$type}_id) || ' (' || {$type}_id ||') ' || name_alias"=>'%'.$term.'%');
 	$name = ( $GLOBALS['AG_DEMO_MODE'] and ($type==AG_MAIN_OBJECT_DB))
 		? "'XXXXXX'"
 		: $type . '_name('.$type.'_id)';
