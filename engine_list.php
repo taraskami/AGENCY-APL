a779be4ef2c106b7c5e7500c339b39f821ac28e7
Ken Tanzer <ken.tanzer@gmail.com>
2012-04-27 15:03:48 -0700

    Fix a/an grammar on add link in list

--- engine_list.php
+++ engine_list.php
@@ -223,7 +223,7 @@ function list_generic($control,$def,$control_array_variable='',&$REC_NUM)
  	}
 
 	if ($control['list']['display_add_link'] or ($total<1)) {
-		$add_link = link_engine(array('object'=>$object,'action'=>'add'),'Add a '.$def['singular']);
+		$add_link = link_engine(array('object'=>$object,'action'=>'add'),'Add ' . aan($def['singular']) .' '.$def['singular']);
 	}
 
       if ($total < 1) {
