17f86748ae1a638683561e30256c6e64cf91af14
Ken Tanzer <ken.tanzer@gmail.com>
2012-07-28 23:52:42 -0700

    Add location to calendar appointments, and move description to top

--- database/pg/client/create.tbl_calendar_appointment.sql
+++ database/pg/client/create.tbl_calendar_appointment.sql
@@ -1,12 +1,14 @@
 CREATE TABLE tbl_calendar_appointment (
 	calendar_appointment_id			SERIAL PRIMARY KEY,
 	calendar_id				INTEGER NOT NULL REFERENCES tbl_calendar ( calendar_id ),
+	description				VARCHAR(90),
 	client_id				INTEGER REFERENCES tbl_client ( client_id ),
 	event_start				TIMESTAMP(0) NOT NULL CHECK (event_start::text ~ '(00|15|30|45):00$'),
 	event_end				TIMESTAMP(0) NOT NULL CHECK ( (event_end::text ~ '(00|15|30|45|(23:59)):00$') --for the last time-slot of the day
 										AND event_end > event_start),
-	description				VARCHAR(90),
 	calendar_appointment_resolution_code	VARCHAR(10) REFERENCES tbl_l_calendar_appointment_resolution ( calendar_appointment_resolution_code ),
+	location_code			REFERENCES tbl_l_location (location_code),
+	location_text			TEXT,
 	comments				TEXT,
 	allow_overlap			BOOLEAN NOT NULL DEFAULT FALSE,
 --system fields
@@ -26,6 +28,7 @@ CREATE TABLE tbl_calendar_appointment (
 CREATE INDEX index_tbl_calendar_appointment_calendar_id ON tbl_calendar_appointment ( calendar_id );
 CREATE INDEX index_tbl_calendar_appointment_client_id ON tbl_calendar_appointment ( client_id );
 CREATE INDEX index_tbl_calendar_appointment_event_start ON tbl_calendar_appointment ( event_start );
+CREATE INDEX index_tbl_calendar_appointment_location_code ON tbl_calendar_appointment ( location_code );
 CREATE INDEX index_tbl_calendar_appointment_calendar_appointment_resolution_code ON tbl_calendar_appointment ( calendar_appointment_resolution_code );
 CREATE INDEX index_tbl_calendar_appointment_client_id_event_start ON tbl_calendar_appointment ( client_id,event_start );
 CREATE INDEX index_tbl_calendar_appointment_event_start_client_id ON tbl_calendar_appointment ( event_start,client_id );
