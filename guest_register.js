fbed9280de04398e0c58c322d537f129cc465ad8
Ken Tanzer <ken.tanzer@gmail.com>
2013-01-30 13:42:32 -0800

    More tweaks in guest registration.

--- guest_register.js
+++ guest_register.js
@@ -1,13 +1,15 @@
 function inputsValidate() {
 	var unit = $('div.guestLoginUnit');
 	var secret = $('div.guestLoginSecret');
+
+	if (! ((unit.length==1) && secret.length==1)) {
+		// Stop if we're on the wrong page
+		return true;
+	}
+
 	var goButton = $('div.guestLoginGo');
 	//var secred_regex=/([0-9]{1,2}[\-\/]?){2}[0-9]{2,4}/; // for a full dob
 	var secret_regex = /^([0-9]{4})$/; // 4 year DOB
-
-	if (!inputsValidate.validUnits) {
-		inputsValidate.validUnits= jQuery.parseJSON( $( '#housingUnitCodes' ).html() );
-	}
 
 	var our_form = $(goButton).closest('form');
 
@@ -18,19 +20,31 @@ function inputsValidate() {
 	var check_secret = (secret_val !== undefined) && secret_val.match(secret_regex);
 	if ( (check_unit) && check_secret) {
 		var loading = $('<img></img>').attr('src','images/loading.gif');
-		$(unit).find('input').removeAttr('disabled').after(loading);
+		$(unit).find('input').removeAttr('disabled');
+		$(unit).after(loading);
 		$(our_form).submit();
 		return true;
 	} else if (check_unit) {
 		var keyboard=$(unit).find('input').keyboard().getkeyboard();
 		keyboard.destroy();
-		$(unit).find('input').attr('disabled','disabled');
+//		$(unit).find('input').attr('disabled','disabled');
 		$(secret).show().find('input').focus();
 		return false;
 	} 
 }
 
 $( function() {
+
+	inputsValidate.validUnits= jQuery.parseJSON( $( '#housingUnitCodes' ).html() );
+
+  $('div.guestLoginUnit input').focus( function() {
+		inputsValidate();
+		var inp = $('div.guestLoginUnit').find('input');
+		if ( ($(inp).val() !== undefined) && jQuery.inArray($(inp).val(), inputsValidate.validUnits) > -1) { 
+			$(inp).val('');
+		}
+	});
+
 	$('.calTodayLink').remove();
 	var params = {
 		alwaysOpen: false,
