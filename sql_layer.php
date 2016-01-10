a4ca6a15c29bb6adb2d01347683223e3f6bc7654
Ken Tanzer <ken.tanzer@gmail.com>
2016-01-10 14:14:32 -0800

    Much better version of sql_to_php_array(), works with arrays Copied from http://php.net/manual/en/ref.pgsql.php, comment posted by ChrisKL

--- sql_layer.php
+++ sql_layer.php
@@ -867,12 +867,53 @@ function sql_field_names($res)
 	return $fields;
 }
 
-function sql_to_php_array($string)
-{
-	if (strpos($string,'","')) {
-		return explode('","',trim($string,'{","}'));
-	}
-	return explode(',',trim($string,'{","}'));
+function sql_to_php_array($dbarr) {
+
+	// Copied from http://php.net/manual/en/ref.pgsql.php, posted by ChrisKL
+
+    /**
+     * Change a db array into a PHP array
+     * @param $dbarr String representing the DB array
+     * @return A PHP array
+     */
+
+        // Take off the first and last characters (the braces)
+        $arr = substr($dbarr, 1, strlen($dbarr) - 2);
+
+        // Pick out array entries by carefully parsing.  This is necessary in order
+        // to cope with double quotes and commas, etc.
+        $elements = array();
+        $i = $j = 0;        
+        $in_quotes = false;
+        while ($i < strlen($arr)) {
+            // If current char is a double quote and it's not escaped, then
+            // enter quoted bit
+            $char = substr($arr, $i, 1);
+            if ($char == '"' && ($i == 0 || substr($arr, $i - 1, 1) != '\\')) 
+                $in_quotes = !$in_quotes;
+            elseif ($char == ',' && !$in_quotes) {
+                // Add text so far to the array
+                $elements[] = substr($arr, $j, $i - $j);
+                $j = $i + 1;
+            }
+            $i++;
+        }
+        // Add final text to the array
+        $elements[] = substr($arr, $j);
+
+        // Do one further loop over the elements array to remote double quoting
+        // and escaping of double quotes and backslashes
+        for ($i = 0; $i < sizeof($elements); $i++) {
+            $v = $elements[$i];
+            if (strpos($v, '"') === 0) {
+                $v = substr($v, 1, strlen($v) - 2);
+                $v = str_replace('\\"', '"', $v);
+                $v = str_replace('\\\\', '\\', $v);
+                $elements[$i] = $v;
+            }
+        }
+
+        return $elements;
 }
 
 function php_to_sql_array($array)
